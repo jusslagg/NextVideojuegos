2723b9b56bef326ad208911f40a1e0047395ffb4
Jesus Gil <lagg312@gmail.com>
2025-01-19 13:44:00 -0300

    se agregan estilos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,15 +8,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        dark: "#1a202c", // Fondo oscuro
+        dark: "#171717", // Fondo oscuro por defecto
+        lightBackground: "#ffffff", // Fondo blanco para modo claro
         electric: "#00ffff", // Azul eléctrico
+        neonPink: "#ff00ff", // Color neón rosado
+        foreground: "#171717", // Texto principal
+        foregroundLight: "#ededed", // Texto para modo claro
       },
       fontFamily: {
-        sans: ['"Press Start 2P"', "cursive"], // Fuente retro de videojuegos
+        sans: ['"Press Start 2P"', "cursive"], // Fuente retro
       },
       boxShadow: {
         neon: "0 0 10px 2px rgba(0, 255, 255, 0.7)", // Efecto de sombra neón
-        glow: "0 0 20px 5px rgba(255, 0, 255, 0.6)", // Efecto glow para botones
+        glow: "0 0 20px 5px rgba(255, 0, 255, 0.6)", // Efecto glow
       },
     },
   },
