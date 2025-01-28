92e2727e5e6cd9f530fdbd83bd98f8ba59e2e266
Jesus Gil <lagg312@gmail.com>
2025-01-28 19:56:30 -0300

    se dio permisos de cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https", // Asegúrate de que el protocolo sea https
+        hostname: "res.cloudinary.com", // Dominio de Cloudinary
+        port: "",
+        pathname: "/**", // Puedes especificar el pathname si lo necesitas
+      },
+    ],
+  },
 };
 
 export default nextConfig;
