d8143898ea85b314af4839d4befed51ab9112edb
Jesus Gil <lagg312@gmail.com>
2025-02-05 09:17:51 -0300

    configuración de slug

--- app/api/product/[slug]/route.js
+++ app/api/product/[slug]/route.js
@@ -1,18 +1,39 @@
 import { db } from "@/app/context/configFirebase";
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { NextResponse } from "next/server";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
-export async function GET(request, { params }) {
-  const { slug } = await params;
+export async function GET(req, { params }) {
+  const { slug } = params; // Extrae el slug de los parámetros de la URL
 
-  const ref = collection(db, "products");
-  const q = query(ref, where("slug", "==", slug));
+  if (!slug) {
+    return new Response("El slug es necesario para realizar la solicitud", {
+      status: 400,
+    });
+  }
 
-  const querySnapshot = await getDocs(q);
-  const data = querySnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
+  try {
+    // Consulta a Firestore para obtener el producto con el slug especificado
+    const productsRef = collection(db, "products");
+    const q = query(productsRef, where("slug", "==", slug));
+    const querySnapshot = await getDocs(q);
 
-  return NextResponse.json(data);
+    // Si no se encuentra el producto
+    if (querySnapshot.empty) {
+      return new Response("Producto no encontrado", { status: 404 });
+    }
+
+    // Si encontramos el producto, lo devolvemos
+    const product = querySnapshot.docs[0].data();
+    return new Response(JSON.stringify(product), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    // Si ocurre un error en la consulta
+    console.error("Error al obtener el producto:", error);
+    return new Response(`Error al obtener el producto: ${error.message}`, {
+      status: 500,
+    });
+  }
 }
